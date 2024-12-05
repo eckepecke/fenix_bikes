90e36d8b653e5e63a04b7692e3930985d61ce874
caas23 <caas23@student.bth.se>
2024-12-05 18:32:01 +0100

    Added functionality and temp data to /cities and /city/:city routes

--- services/admin-web-ui/src/App.tsx
+++ services/admin-web-ui/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import City from "./components/City";
 import Map from "./components/Map";
 import Home from "./views/Home";
 import Bikes from "./views/Bikes";
@@ -18,9 +19,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/bikes" element={<Bikes />} />
           <Route path="/cities" element={<Cities />} />
-          <Route path="/users" element={<Users />} />
+          <Route path="/city/:city" element={<City />} />
           <Route path="/maps" element={<Maps />} />
           <Route path="/map/:city" element={<Map />} />
+          <Route path="/users" element={<Users />} />
         </Routes>
       </main>
       <Footer />
---
+++ services/admin-web-ui/src/components/City/tempRulesData.tsx
@@ -0,0 +1,22 @@
+const Rule1 = {
+  _id: "R001",
+  description: "This is rule #1",
+};
+
+const Rule2 = {
+  _id: "R002",
+  description: "This is rule #2",
+};
+
+const Rule3 = {
+  _id: "R003",
+  description: "This is rule #3",
+};
+
+const tempDataRules = [
+  { rule_id: Rule1._id, description: Rule1.description },
+  { rule_id: Rule2._id, description: Rule2.description },
+  { rule_id: Rule3._id, description: Rule3.description },
+];
+
+export { Rule1, Rule2, Rule3, tempDataRules };
