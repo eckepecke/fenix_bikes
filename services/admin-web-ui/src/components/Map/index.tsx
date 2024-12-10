7a187c16816b94f0059d2fbefb144e68d5adb491
August Levinson <118256272+920523@users.noreply.github.com>
2024-12-10 23:10:31 +0100

    Added icons to UI services

--- services/admin-web-ui/src/components/Map/index.tsx
+++ services/admin-web-ui/src/components/Map/index.tsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { MapContainer, Marker, Popup, TileLayer, GeoJSON } from "react-leaflet";
 import L from "leaflet";
-import scooterIcon from "/src/assets/scooter-icon.png";
+import scooterIcon from "/src/assets/scooter-pin.png";
+import chargingIcon from "/src/assets/charging-station.png";
+import parkingIcon from "/src/assets/parking-spot.png";
 import "./index.css";
 
 
@@ -10,6 +12,9 @@ const Map: React.FC = () => {
 	const { city } = useParams<{ city: string }>();
 	const [cityBorders, setCityBorders] = useState<any>(null);
 	const [cityCenter, setCityCenter] = useState<[number, number] | null>(null);
+	const [cityEast, setCityEast] = useState<[number, number] | null>(null);
+	const [cityWest, setCityWest] = useState<[number, number] | null>(null);
+	const [citySouth, setCitySouth] = useState<[number, number] | null>(null);
 
 	// ett sätt att bibehålla å, ä och ö när vi skriver ut stadens namn,
 	// nackdel: behöva lägga in städer manuellt, fördel: smidig lösning
@@ -22,9 +27,23 @@ const Map: React.FC = () => {
 
 	const scooterMarker = L.icon({
 		iconUrl: scooterIcon,
+		iconSize: [50, 50],
+		iconAnchor: [25, 50],
+		popupAnchor: [0, -40],
+	});
+
+	const chargingStationMarker = L.icon({
+		iconUrl: chargingIcon,
 		iconSize: [30, 30],
 		iconAnchor: [15, 30],
-		popupAnchor: [0, -30],
+		popupAnchor: [1, -20],
+	});
+
+	const parkingSpotMarker = L.icon({
+		iconUrl: parkingIcon,
+		iconSize: [30, 30],
+		iconAnchor: [15, 30],
+		popupAnchor: [1, -20],
 	});
 
 	useEffect(() => {
@@ -44,6 +63,9 @@ const Map: React.FC = () => {
 				if (data?.[0]?.geojson) {
 					setCityBorders(data[0].geojson);
 					setCityCenter([parseFloat(data[0].lat), parseFloat(data[0].lon)]);
+					setCityEast([parseFloat(data[0].lat) + 0.003, parseFloat(data[0].lon) + 0.009]);
+					setCityWest([parseFloat(data[0].lat) + 0.003, parseFloat(data[0].lon) - 0.011]);
+					setCitySouth([parseFloat(data[0].lat) - 0.007, parseFloat(data[0].lon) + 0.011]);
 				} else {
 					console.error("No city data found.");
 				}
@@ -83,7 +105,7 @@ const Map: React.FC = () => {
 						style={{
 							color: "#1A4D30", // --color-green-darker
 							weight: 1.5,
-							fillOpacity: 0.15,
+							fillOpacity: 0.0,
 						}}
 					/>
 				)};
@@ -94,6 +116,32 @@ const Map: React.FC = () => {
 						Men med custom ikoner för vardera del.
 					</Popup>
 				</Marker>
+				<Marker position={cityEast} icon={scooterMarker}>
+					<Popup>
+						<div className="popup-content">
+							<h2 className="popup-id">#8b8436</h2>
+							<p><span className="bold">Available: </span>True</p>
+							<p><span className="bold">Speed: </span>0 km/h</p>
+							<p><span className="bold">Battery level: </span>82 %</p>
+						</div>
+					</Popup>
+				</Marker>
+				<Marker position={cityWest} icon={chargingStationMarker}>
+					<Popup>
+						<div className="popup-content">
+							<h2 className="popup-id">#S001</h2>
+							<p><span className="bold">Available plugs: </span>1/3</p>
+						</div>
+					</Popup>
+				</Marker>
+				<Marker position={citySouth} icon={parkingSpotMarker}>
+					<Popup>
+						<div className="popup-content">
+							<h2 className="popup-id">#P001</h2>
+						</div>
+					</Popup>
+				</Marker>
+
 			</MapContainer>
 		</div>
 	);
