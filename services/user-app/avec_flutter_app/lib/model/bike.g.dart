1bc882b10a470368ee5fcf4a9cb5751db1580e1a
Viktoria Arvidsson <viav23@student.bth.se>
2024-12-12 15:16:19 +0100

    Added city-filter on showing bikes on the map.

--- services/user-app/avec_flutter_app/lib/model/bike.g.dart
+++ services/user-app/avec_flutter_app/lib/model/bike.g.dart
@@ -9,9 +9,11 @@ part of 'bike.dart';
 Bike _$BikeFromJson(Map<String, dynamic> json) => Bike(
       location: json['location'] as List<dynamic>,
       status: Status.fromJson(json['status'] as Map<String, dynamic>),
+      cityName: json['city_name'] as String? ?? 'Okänd',
     );
 
 Map<String, dynamic> _$BikeToJson(Bike instance) => <String, dynamic>{
       'location': instance.location,
       'status': instance.status.toJson(),
+      'city_name': instance.cityName,
     };
