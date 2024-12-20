eb3c6ebf0fe57e08905e7af97afa66f174a6fd57
Viktoria Arvidsson <viav23@student.bth.se>
2024-12-20 17:12:34 +0100

    Added testroute but not the function in bike

--- services/simulation/simulation.js
+++ services/simulation/simulation.js
@@ -15,11 +15,21 @@ import data from './trips/lund-trip.json' assert { type: "json" };
 // console.log(data[0].trip1.coords[0]);
 
 for (let coord in data[0].trip1.coords) {
-    setTimeout(() => {
+    setTimeout(async () => {
         console.log(data[0].trip1.coords[coord]);
+        await fetch("localhost:1337/test/update/location", {
+            method: "POST",
+            body: JSON.stringify({
+                bike_id: "B0024",
+                location: data[0].trip1.coords[coord]
+
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        });
     }, coord * 2000);
     // console.log(data[0].trip1.coords[coord]);
-
 };
 
 
