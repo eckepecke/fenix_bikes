23ec3f0d70a321df81d73bcc33567d41ea2c8bf7
Viktoria Arvidsson <viav23@student.bth.se>
2024-12-20 14:48:36 +0100

    Added simulation-folder and started creating trips

---
+++ services/simulation/decoder.js
@@ -0,0 +1,5 @@
+var polyline = require('@mapbox/polyline');
+
+let tripCords = polyline.decode("a}~rIeiooA@@@ABBh@oCJUa@iDC]?W@S@e@FgA?OHeBFaABs@XaANJj@\\RHDGXMv@g@^WJINHHKLkAHc@h@_B|@iAf@GTIz@Wz@YPEN@h@Qp@SNE");
+
+console.log(tripCords);
