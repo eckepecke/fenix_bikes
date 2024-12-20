75d90e757105ceb4e1d9f0e2486080378d2f8807
Viktoria Arvidsson <viav23@student.bth.se>
2024-12-20 17:01:23 +0100

    Locations on timer

--- services/simulation/simulation.js
+++ services/simulation/simulation.js
@@ -1,7 +1,26 @@
-const { io } = require("socket.io-client");
+// const { io } = require("socket.io-client");
 
-const socket = io('localhost:1337');
+// const socket = io('localhost:1337');
 
-socket.on("connect", () => {
-    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
-});
+
+// socket.on("connect", () => {
+//     console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+// });
+
+// socket.on("disconnect", () => {
+//     console.log(socket.id); // undefined
+// });
+
+import data from './trips/lund-trip.json' assert { type: "json" };
+// console.log(data[0].trip1.coords[0]);
+
+for (let coord in data[0].trip1.coords) {
+    setTimeout(() => {
+        console.log(data[0].trip1.coords[coord]);
+    }, coord * 2000);
+    // console.log(data[0].trip1.coords[coord]);
+
+};
+
+
+
