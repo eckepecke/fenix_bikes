821726c689ee66a36638391e8c9f11d310941e32
Viktoria Arvidsson <viav23@student.bth.se>
2024-12-21 16:19:46 +0100

    Simulation back

--- services/simulation/simulation.js
+++ services/simulation/simulation.js
@@ -11,8 +11,8 @@
 //     console.log(socket.id); // undefined
 // });
 
-// import data from './trips/lund-trip.json' assert { type: "json" };
-// // console.log(data[0].trip1.coords[0]);
+import data from './trips/lund-trip.json' assert { type: "json" };
+// console.log(data[0].trip1.coords[0]);
 
 /* global io */
 
@@ -22,24 +22,24 @@
 //     console.log(socket.id); // x8WIv7-mJelg7on_ALbx
 // });
 
-// for (let coord in data[0].trip1.coords) {
-//     setTimeout(() => {
-//         console.log(data[0].trip1.coords[coord]);
-//         let coordinates = data[0].trip1.coords[coord];
-//         fetch("http://localhost:1337/test/location/update", {
-//             method: "POST",
-//             body: JSON.stringify({
-//                 bike_id: "B0024",
-//                 location: coordinates
+for (let coord in data[0].trip1.coords) {
+    setTimeout(() => {
+        console.log(data[0].trip1.coords[coord]);
+        let coordinates = data[0].trip1.coords[coord];
+        fetch("http://localhost:1337/test/location/update", {
+            method: "POST",
+            body: JSON.stringify({
+                bike_id: "B0024",
+                location: coordinates
 
-//             }),
-//             headers: {
-//                 "Content-type": "application/json; charset=UTF-8"
-//             }
-//         });
-//     }, coord * 5000);
-//     // console.log(data[0].trip1.coords[coord]);
-// };
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        });
+    }, coord * 5000);
+    // console.log(data[0].trip1.coords[coord]);
+};
 
 
 
