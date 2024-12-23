e873a3cc0ee66563b943096b03bef92b797f12ea
August Levinson <118256272+920523@users.noreply.github.com>
2024-12-23 13:00:53 +0100

    Added users to admin-ui

--- services/admin-web-ui/src/views/Users/index.tsx
+++ services/admin-web-ui/src/views/Users/index.tsx
@@ -1,13 +1,53 @@
 import React, { useEffect } from "react";
-// import "./index.css";
+import { useNavigate } from "react-router-dom";
+import "./index.css";
 
 const Users: React.FC = () => {
 	useEffect(() => {
 		document.title = "Users - Avec";
-}, []);
+	}, []);
+
+	const navigate = useNavigate();
+
+	const displayUser = (user: string) => {
+		navigate(`/user/${user}`);
+	};
+
+	const fetchedUsers = [
+		{
+			_id: "676529d83fcc9576927e5c74",
+			name: "August Levinson",
+			payment_method: "prepaid",
+			password: "1234",
+			email: "augustlevinson@gmail.com",
+			banned: false,
+			completed_trips: [],
+		},
+		{
+			_id: "6765617d4cd3ac975e5ca6fd",
+			name: "Test User",
+			payment_method: "prepaid",
+			password: "1234",
+			email: "test@email.se",
+			banned: false,
+			completed_trips: ["T0013", "T0016", "T0017"],
+			user_id: "U0011",
+		},
+	];
+
 	return (
 		<div>
 			<h1>Users</h1>
+			<ul className="user-list">
+				{fetchedUsers.map((user, index) => (
+					<li
+						key={user._id}
+						onClick={() => displayUser(user._id)}
+						className={index === fetchedUsers.length - 1 ? "li-last-user" : ""}>
+						{user.name}
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
--- services/rest-api/src/routes/getDataRoutes.js
+++ services/rest-api/src/routes/getDataRoutes.js
@@ -1,76 +1,102 @@
-import express from 'express';
-import { getCities } from '../../../db/cities.js';
-import { getBikes } from '../../../db/bikes.js';
-import { getParking } from '../../../db/parkingZones.js';
-import { getUsers } from '../../../db/users.js';
-import { getTrips } from '../../../db/trips.js';
-import bikeManager from "../../../bike-logic/bikeManager.js"
-import bike from "../../../bike-logic/bike.js"
+import express from "express";
+import { getCities } from "../../../db/cities.js";
+import { getBikes } from "../../../db/bikes.js";
+import { getParking, getAllParking } from "../../../db/parkingZones.js";
+import { getChargingStations, getAllChargingStations } from "../../../db/chargingStations.js";
+import { getUsers } from "../../../db/users.js";
+import { getTrips } from "../../../db/trips.js";
+import bikeManager from "../../../bike-logic/bikeManager.js";
+import bike from "../../../bike-logic/bike.js";
 
 const router = express.Router();
 
 // GET /bikes
 router.get("/", async (req, res) => {
-    res.json("hej getData routes");
+	res.json("hej getData routes");
 });
 
 router.get("/all/cities", async (req, res) => {
-    const result = await getCities();
-    res.json(result);
+	const result = await getCities();
+	res.json(result);
 });
-
 
 // GET /all/bikes
 router.get("/all/bikes", async (req, res) => {
-    const result = await bikeManager.getAllBikes();
+	const result = await bikeManager.getAllBikes();
 
-    res.json(result);
+	res.json(result);
 });
 
 router.get("/all/users", async (req, res) => {
-    const result = await getUsers();
-    res.json(result);
+	const result = await getUsers();
+	res.json(result);
 });
 
 router.get("/all/trips", async (req, res) => {
-    const result = await getTrips();
-    res.json(result);
+	const result = await getTrips();
+	res.json(result);
 });
 
 // för /bikes-vyn i admin
 router.get("/all/bikes/pagination", async (req, res) => {
-    const page = req.query.page || 1;
-    const search = req.query.search || "";
-    const limit = 5; // visa 5 cyklar i taget
-    const skip = (page - 1) * limit;
-  
-    // om sökord finns används inbyggda regex och case-insensitive för att söka i db
-    const filter = search ? { bike_id: { $regex: search, $options: "i" } } : {};
+	const page = req.query.page || 1;
+	const search = req.query.search || "";
+	const limit = 5; // visa 5 cyklar i taget
+	const skip = (page - 1) * limit;
 
-    try {
-      const bikes = await bikeManager.getBikesPagination(filter, skip, limit);
-      const totalBikes = await bikeManager.countBikesPagination(filter); // totala antal cyklar baserat på sökning
-      const totalPages = Math.ceil(totalBikes / limit);
-  
-      res.json({ bikes, totalPages });
-    } catch (error) {
-      console.error("Error fetching bikes:", error);
-      res.status(500).send("Error fetching bikes");
-    }
+	// om sökord finns används inbyggda regex och case-insensitive för att söka i db
+	const filter = search ? { bike_id: { $regex: search, $options: "i" } } : {};
+
+	try {
+		const bikes = await bikeManager.getBikesPagination(filter, skip, limit);
+		const totalBikes = await bikeManager.countBikesPagination(filter); // totala antal cyklar baserat på sökning
+		const totalPages = Math.ceil(totalBikes / limit);
+
+		res.json({ bikes, totalPages });
+	} catch (error) {
+		console.error("Error fetching bikes:", error);
+		res.status(500).send("Error fetching bikes");
+	}
 });
 
 router.get("/all/bikes/in/city/:city", async (req, res) => {
-    const city = req.params.city;
-    const result = await bikeManager.getAllBikesInCity(city);
+	const city = req.params.city;
+	const result = await bikeManager.getAllBikesInCity(city);
 
-    res.json(result);
+	res.json(result);
 });
 
 router.get("/certain/bike/:bike_id", async (req, res) => {
-    const bikeId = req.params.bike_id;
-    const result = await bike.reportState(bikeId);
+	const bikeId = req.params.bike_id;
+	const result = await bike.reportState(bikeId);
 
+	res.json(result);
+});
+
+// GET /city/:city/parking-zones
+router.get("/city/:city/parking-zones", async (req, res) => {
+    const city = req.params.city;
+    const result = await getParking(city);
     res.json(result);
+});
+
+// GET /all/parking-zones
+router.get("/all/parking-zones", async (req, res) => {
+	const result = await getAllParking();
+	res.json(result);
+});
+
+// GET /city/:city/charging-stations
+router.get("/city/:city/charging-stations", async (req, res) => {
+	const city = req.params.city;
+	const result = await getChargingStations(city);
+	res.json(result);
+});
+
+// GET /all/charging-stations
+router.get("/all/charging-stations", async (req, res) => {
+	const result = await getAllChargingStations();
+	res.json(result);
 });
 
 export default router;
