a9ca4db337b315f8cf7eaf9d4ac8f8f3c0a9b421
Viktoria Arvidsson <viav23@student.bth.se>
2024-12-28 16:20:06 +0100

    Added first tests and bikes to testdatabase

--- services/rest-api/tests/get.test.js
+++ services/rest-api/tests/get.test.js
@@ -5,11 +5,22 @@ import app from '../src/app.js';
 
 
 describe("GET /", () => {
-    it("should greet the world when no name is provided", async () => {
+    it("should print greeting", async () => {
         const res = await request(app)
             .get("/")
             .expect(200);
 
         expect(res.text).toBe("Greetings, friend of Fenix!");
+    });
+});
+
+describe("GET /get/all/bikes", () => {
+    it("should return all bikes in database", async () => {
+        const res = await request(app)
+            .get("/get/all/bikes")
+            .expect('Content-Type', /json/)
+            .expect(200);
+
+        expect(res.body).toHaveLength(10);
     });
 });
