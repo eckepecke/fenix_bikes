9c59e3e94ce4cd0c2649b1852294c6a362544400
August Levinson <118256272+920523@users.noreply.github.com>
2025-01-03 10:59:27 +0100

    Moved db population to test file

--- services/rest-api/tests/get.test.js
+++ services/rest-api/tests/get.test.js
@@ -1,7 +1,56 @@
 import bike from '../../bike-logic/bike.js';
 import request from 'supertest';
 import app from '../src/app.js';
-// const bike = require('../../bike-logic/bike.js');
+import { MongoClient } from 'mongodb';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let connection;
+let db;
+
+beforeAll(async () => {
+    // Set the MongoDB URI and database name for testing
+    global.__MONGO_URI__ = "mongodb://localhost:27017";
+    global.__MONGO_DB_NAME__ = "test";
+
+    connection = await MongoClient.connect(global.__MONGO_URI__, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    db = await connection.db(global.__MONGO_DB_NAME__);
+
+    // Clear the collections
+    await db.collection('bikes').deleteMany({});
+    await db.collection('users').deleteMany({});
+    await db.collection('trips').deleteMany({});
+    await db.collection('parking_zones').deleteMany({});
+    await db.collection('cities').deleteMany({});
+    await db.collection('charging_stations').deleteMany({});
+
+    // Read and parse JSON files
+    const bikes = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../testdb/addbikes.json'), 'utf8'));
+    const users = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../testdb/addusers.json'), 'utf8'));
+    const trips = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../testdb/addtrips.json'), 'utf8'));
+    const parkingZones = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../testdb/addparking.json'), 'utf8'));
+    const cities = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../testdb/addcities.json'), 'utf8'));
+    const chargingStations = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../testdb/addcharging.json'), 'utf8'));
+
+    // Populate the test database
+    await db.collection('bikes').insertMany(bikes);
+    await db.collection('users').insertMany(users);
+    await db.collection('trips').insertMany(trips);
+    await db.collection('parking_zones').insertMany(parkingZones);
+    await db.collection('cities').insertMany(cities);
+    await db.collection('charging_stations').insertMany(chargingStations);
+});
+
+afterAll(async () => {
+    await connection.close();
+});
 
 
 describe("GET /", () => {
