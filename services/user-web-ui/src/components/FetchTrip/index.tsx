297a6d3be00b8090c289a577959670fff36a1c8f
August Levinson <118256272+920523@users.noreply.github.com>
2025-01-08 11:53:47 +0100

    Added cost fetching in user UI

--- services/user-web-ui/src/components/FetchTrip/index.tsx
+++ services/user-web-ui/src/components/FetchTrip/index.tsx
@@ -6,17 +6,29 @@ interface Trip {
   end_time: Date;
   start_location: Array<number>;
   end_location: Array<number>;
+  cost?: number;
 }
 
 // Fetch trip data
 const FetchTrip = async (tripId: string): Promise<Trip | null> => {
   try {
-    const response = await fetch(`http://localhost:1337/get/trip/${tripId}`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+    const tripResponse = await fetch(`http://localhost:1337/get/trip/${tripId}`);
+    if (!tripResponse.ok) {
+      throw new Error(`HTTP error! status: ${tripResponse.status}`);
     }
-    const trip: Trip = await response.json();
 
+    const trip: Trip = await tripResponse.json();
+
+    const tripCostResponse = await fetch(`http://localhost:1337/trip/calculate/${tripId}`);
+
+    if (!tripCostResponse.ok) {
+      throw new Error(`HTTP error! status: ${tripCostResponse.status}`);
+    }
+
+    const tripCost = await tripCostResponse.json();
+    trip.cost = tripCost;
+    
+    
     // Check if the trip ID matches
     return trip.trip_id === tripId ? trip : null;
   } catch (error) {
