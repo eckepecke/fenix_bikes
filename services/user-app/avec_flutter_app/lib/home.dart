0f8be918f73998e71f15fca811aa44445db439ba
Viktoria Arvidsson <viav23@student.bth.se>
2025-01-14 08:45:37 +0100

    Google sign in working and dockerfile created

--- services/user-app/avec_flutter_app/lib/home.dart
+++ services/user-app/avec_flutter_app/lib/home.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'map.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'model/user.dart';
 
 // Home page
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  MyHomePage({super.key, required this.user});
+
+  GoogleSignInAccount user;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -13,6 +19,26 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   TextEditingController cityController = TextEditingController();
   City? selectedCity;
+  User? fetchedUser;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUser();
+  }
+
+  Future<void> _fetchUser() async {
+    await http.post(
+      Uri.parse('http://localhost:1337/auth/app/user'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'userName': widget.user.displayName!,
+        'userEmail': widget.user.email
+      }),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,30 +70,30 @@ class _MyHomePageState extends State<MyHomePage> {
               )
             : MapPage(
                 selectedCity: selectedCity!.label,
-              ),
-        endDrawer: const Drawer(
+                userEmail: widget.user.email),
+        endDrawer: Drawer(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            SizedBox(
+            const SizedBox(
               height: 30,
             ),
-            Text(
+            const Text(
               'Här kommer mer information',
               textAlign: TextAlign.left,
             ),
-            SizedBox(
+            const SizedBox(
               height: 30,
             ),
-            Text('Användare'),
-            SizedBox(
+            Text(widget.user.displayName!),
+            const SizedBox(
               height: 30,
             ),
-            Text('Tidigare resor'),
-            SizedBox(
+            const Text('Tidigare resor'),
+            const SizedBox(
               height: 30,
             ),
-            Text('Byt stad'),
+            const Text('Byt stad'),
           ],
         )));
   }
---
+++ services/user-app/avec_flutter_app/lib/model/user.dart
@@ -0,0 +1,23 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user.g.dart';
+
+// Converts json object Bike to dart object Bike
+
+@JsonSerializable(explicitToJson: true)
+class User {
+  final String name;
+  final String email;
+
+  @JsonKey(name: 'user_id')
+  final String userId;
+
+  const User({required this.name, required this.email, required this.userId});
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UserToJson(this);
+
+  @override
+  String toString() => 'User{name: $name, email: $email, userId: $userId}';
+}
